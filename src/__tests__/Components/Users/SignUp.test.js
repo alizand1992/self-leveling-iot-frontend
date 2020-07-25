59752fa3c4a33c12dcdb2b2a0948ef6fc0d2f3d8
Ali Zand <ali.zand9224@gmail.com>
2020-07-25 09:41:35 -0700

    RF-8 Finish up tests for Sign Up

--- src/__tests__/Components/Users/SignUp.test.js
+++ src/__tests__/Components/Users/SignUp.test.js
@@ -41,4 +41,45 @@ describe('Sign Up', () => {
     expect(wrapper.find('Button').text()).toBe('Sign Up');
   });
 
+  describe('onChange', () => {
+    describe('first_name change', () => {
+      wrapper.find({ id: 'first_name' }).simulate('change', { target: { value: 'John' } });
+
+      it('updates the first_name state', () => {
+        expect(wrapper.state('first_name')).toBe('John');
+      });
+    });
+
+    describe('last_name change', () => {
+      wrapper.find({ id: 'last_name' }).simulate('change', { target: { value: 'Smith' } });
+
+      it('updates the last_name state', () => {
+        expect(wrapper.state('last_name')).toBe('Smith');
+      });
+    });
+
+    describe('email change', () => {
+      wrapper.find({ id: 'email' }).simulate('change', { target: { value: 'john.smith@gmail.com' } });
+
+      it('updates the email state', () => {
+        expect(wrapper.state('email')).toBe('john.smith@gmail.com');
+      });
+    });
+
+    describe('password change',  () => {
+      wrapper.find({ id: 'password' }).simulate('change', { target: { value: 'password123' } });
+
+      it('updates the password state', () => {
+        expect(wrapper.state('password')).toBe('password123');
+      });
+    });
+
+    describe('confirm_password change',  () => {
+      wrapper.find({ id: 'confirm_password' }).simulate('change', { target: { value: 'password123' } });
+
+      it('updates the confirm_password state', () => {
+        expect(wrapper.state('confirm_password')).toBe('password123');
+      });
+    });
+  });
 });
