5b2e5a72ef5b4b33d12df14d01969bd70e8c5506
flyingboat505 <abhilashmunnangi@gmail.com>
2020-10-11 14:16:37 -0700

    RF-13 Meeting Changes -

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import Container from 'react-bootstrap/Container';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Loading from './Components/Common/Loading';
 import { getUserData } from './Util/Ajax/Users';
@@ -13,8 +13,7 @@ import { bindActionCreators } from 'redux';
 import { signUserIn, signUserOut } from './actions/Users';
 import { connect } from 'react-redux';
 import ProtectedRoute from './Components/Common/ProtectedRoute';
-//import Navbar from './Components/Navbar';
-import { Navbar,Nav ,Button,NavDropdown,Form,FormControl} from 'react-bootstrap';
+import Menu from './Components/Common/Menu';
 
 
 class App extends React.Component {
@@ -52,38 +51,9 @@ class App extends React.Component {
     const EditNotification = React.lazy(() => import('./Components/Notifications/Edit'));
 
     return (
-     <div>
-       <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>
-     </div>
-      /*<Router>
+      <Router>
         <Container>
-          <Link to="/user/sign_in">Sign In</Link> <br />
-          <Link to="/user/sign_up">Sign Up</Link> <br />
-          <Link to="/user/sign_out">Sign Out</Link> <br />
-          <Link to="/user/profile">Profile</Link> <br />
-          <Link to="/notifications">Notifications</Link> <br />
-          <Link to="/notifications/new">Create Notification</Link> <br />
-
+          <Menu />
           <Suspense fallback={<Loading/>}>
             <Switch>
               <Route path="/user/sign_out">
@@ -110,7 +80,7 @@ class App extends React.Component {
             </Switch>
           </Suspense>
         </Container>
-      </Router>*/
+      </Router>
     );
   }
 }
